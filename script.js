13b7bffc0c103eeebf31794f1c77c8a4670f9813
Paul G <paulgellatly@icloud.com>
2020-05-31 20:16:47 +0100

    wrong letters and play again

--- script.js
+++ script.js
@@ -37,4 +37,79 @@ function displayWord() {
 	}
 }
 
+// Update the wrong letters
+function updateWrongLettersEl() {
+	//Display wrong letters
+	wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+    `;
+
+	// Display parts
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
+
+	// Check if lost
+	if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = 'Sorry. You lost';
+		popup.style.display = 'flex';
+	}
+}
+
+// Show notification
+function showNotification() {
+	notification.classList.add('show');
+
+	setTimeout(() => {
+		notification.classList.remove('show');
+	}, 2000);
+}
+
+// Keydown letter press
+window.addEventListener('keydown', (e) => {
+	if (e.keyCode >= 65 && e.keyCode <= 90) {
+		const letter = e.key;
+
+		if (selectedWord.includes(letter)) {
+			if (!correctLetters.includes(letter)) {
+				correctLetters.push(letter);
+
+				displayWord();
+			} else {
+				showNotification();
+			}
+		} else {
+			if (!wrongLetters.includes(letter)) {
+				wrongLetters.push(letter);
+
+				updateWrongLettersEl();
+			} else {
+				showNotification();
+			}
+		}
+	}
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+	// Empty arrays
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+
+	displayWord();
+
+	updateWrongLettersEl();
+
+	popup.style.display = 'none';
+});
+
 displayWord();
